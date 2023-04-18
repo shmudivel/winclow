ccd8ff6c763311f8cead6c6e151fcadf3afd71d3
shmudivel <82043832+shmudivel@users.noreply.github.com>
2023-04-18 15:24:36 -0700

    comp Feedbacks coners sharp, colors ajusted

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -16,7 +16,7 @@ const FeedbackCard = ({
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
-    className="bg-black-200 p-5 rounded-3xl xs:w-[320px] w-full"
+    className="bg-[#383E56] p-5 xs:w-[320px] w-full"
   >
     <p className="text-white font-black text-[48px]">"</p>
 
@@ -45,10 +45,8 @@ const FeedbackCard = ({
 
 const Feedbacks = () => {
   return (
-    <div className={`mt-12 bg-black-100 rounded-[20px]`}>
-      <div
-        className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
-      >
+    <div className={`mt-12 bg-[#232631]`}>
+      <div className={`bg-[#232631] ${styles.padding} min-h-[300px]`}>
         <motion.div variants={textVariant()}>
           {/* <p className={styles.sectionSubText}>What others say</p> */}
           <h2 className={styles.sectionHeadText}>
