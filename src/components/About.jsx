30c6fa1fd98a6332f2be9929e564e82da735c2c7
shmudivel <82043832+shmudivel@users.noreply.github.com>
2023-04-20 19:16:05 -0700

    icons and 3D model contact

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,7 +24,7 @@ const ServiceCard = ({ index, title, icon }) => (
         <img
           src={icon}
           alt="web-development"
-          className="w-16 h-16 object-contain"
+          className="w-20 h-20 object-contain"
         />
 
         <h3 className="text-white text-[20px] font-bold text-center">
